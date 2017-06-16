ac0e285690300f396e5c26f020ae3c7bc9e96a41
zhaokx3 <1329646981@qq.com>
2017-06-16 10:41:10 +0800

    修改数据库

--- database.sql
+++ database.sql
@@ -13,10 +13,13 @@ CREATE TABLE IF NOT EXISTS `memo`.`user` (
 CREATE TABLE IF NOT EXISTS `memo`.`event` (
 	`eventId` INT NOT NULL AUTO_INCREMENT,
 	`userId` INT NOT NULL,
+	`icon` INT NOT NULL,
+	`category` INT NOT NULL,
     `eventName` VARCHAR(50) NOT NULL,
     `content` TEXT DEFAULT NULL,
     `startTime` VARCHAR(50) NOT NULL,
     `endTime` VARCHAR(50) NOT NULL,
+    `timestamps` VARCHAR(50) NOT NULL,
 	PRIMARY KEY (`eventId`),
     KEY `userId` (`userId`),
     CONSTRAINT `userId` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
